f0520a7148019d274bf2e6a2576b10f2d87d8233
RyanBegnaud <Ryanbegnaud13@gmail.com>
2020-09-04 12:29:03 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    has_one :genre
+    belongs_to :network
+
+    def actors_list
+        self.actors.collect {|actors| actors.full_name}
+    end
 end
